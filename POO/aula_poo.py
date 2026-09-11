0a667bcfa26a8f9060091d4c6abac0e5e90d6ef2
Alberto Lacerda <albertotads@outlook.com>
2026-09-11 19:07:53 -0300

    Aulas

---
+++ POO/aula_poo.py
@@ -0,0 +1,14 @@
+# programacao orientada a objetos
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def apresentar(self):
+        print(f"Olá, meu nome é {self.nome} e eu tenho {self.idade} anos.") 
+        
+        
+pessoa1 = Pessoa("Alice", 30)
+pessoa1.apresentar()  # Olá, meu nome é Alice e eu tenho  
+pessoa2 = Pessoa("Bob", 25)
+pessoa2.apresentar()  # Olá, meu nome é Bob e eu tenho      
---
+++ variaveis/aula1.py
@@ -0,0 +1,19 @@
+nome = "Alice"
+
+print("Olá, " + nome + "! Bem-vinda à aula de Python.")
+estudante = True
+#print(type(nome))
+#print(type(estudante))
+
+
+nome = "Maria"
+idade = 25
+altura = 1.68
+estudante = True
+valor= float("25.45")
+
+print(nome)     # Maria
+print(idade)    # 25
+print(altura)   # 1.68
+print(estudante)# True
+print(valor)    # 25.45
