2515f47535b3850c975cec8279b512cc28938789
Alberto Lacerda <albertotads@outlook.com>
2026-09-11 21:18:26 -0300

    Arquivo sobre variaveis

---
+++ variaveis/tipos.py
@@ -0,0 +1,21 @@
+x=8
+status = "Ativo"
+condicao = True
+complexo = 3 + 4j
+dinheiro = 100.50
+letra ='A'
+conjunto = {1, 2, 3, 4}
+tupla = (1, 2, 3, 4)
+print(type(x))          # <class 'int'>
+print(type(status))     # <class 'str'> 
+print(type(condicao))   # <class 'bool'>
+print(type(complexo))   # <class 'complex'>
+print(type(dinheiro))   # <class 'float'>
+print(type(letra))      # <class 'str'>
+print(type(conjunto))   # <class 'set'>
+print(type(tupla))      # <class 'tuple'>
+print("Tupla:", tupla)  # Tupla: (1, 2, 3, 4)
+print("Conjunto:", conjunto)  # Conjunto: {1, 2, 3, 4}
+dicionario = {"nome": "Alice", "idade": 30, "cidade": "São Paulo"}
+print("Dicionário:", dicionario)  # Dicionário: {'nome': 'Alice', 'idade': 30, 'cidade': 'São Paulo'}
+print(type(dicionario))  # <class 'dict'>
